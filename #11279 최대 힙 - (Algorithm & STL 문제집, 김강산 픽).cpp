1faee0dbce7af52dd11ebf213a2f2d4e5174acd0
Seong Joon Seo <jjune21@naver.com>
2016-08-16 10:44:46 +0900

    #11279 최대 힙 - (Algorithm & STL 문제집, 김강산 픽)

---
+++ #11279 최대 힙 - (Algorithm & STL 문제집, 김강산 픽).cpp
@@ -0,0 +1,39 @@
+/*
+2016.8.16
+서성준
+알고리즘 연습문제 - Algorithm & STL
+#11279 최대 힙 - (Algorithm & STL 문제집, 김강산 픽)
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n,input;
+	cin >> n;
+	vector<int> v;
+	make_heap(v.begin(), v.end());
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		if (input == 0)
+		{
+			if (v.empty())
+				cout << 0 << endl;
+			else {
+				cout << v.front() << endl;
+				pop_heap(v.begin(), v.end());
+				v.pop_back();
+			}
+		}
+		else {
+			v.push_back(input);
+			push_heap(v.begin(), v.end());
+		}
+	}
+	return 0;
+}
